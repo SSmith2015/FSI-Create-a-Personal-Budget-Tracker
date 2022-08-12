4ab1f56f15427a6439e969f5d121abeee36c3927
Sherronda <sherronda82@gmail.com>
2022-08-11 21:31:55 -0400

    Logging users answers and totals

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
-let numberAnswer = parseFloat(stringAnswer)
-console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
+//let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+//let numberAnswer = parseFloat(stringAnswer)
+//console.log(numberAnswer)
